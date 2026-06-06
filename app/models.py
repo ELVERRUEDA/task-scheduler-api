cbd0605181300f69524da422ef2f43dc67652291
ELVERRUEDA <elver.rueda@cun.edu.co>
2026-06-06 15:33:22 -0500

    feat: mejorar main y models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class Tarea(BaseModel):
     titulo: str
     descripcion: Optional[str] = ""
     hora: Optional[str] = ""
+    fecha: Optional[str] = ""
+    completado: Optional[bool] = False
 
 class EstadoCompletado(BaseModel):
     completado: bool
